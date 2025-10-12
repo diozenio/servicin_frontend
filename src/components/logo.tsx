5ecd5f46c71daeedf4092c689192452e08fc37b8
diozenio <d.dionisiobsl@gmail.com>
2025-10-12 11:56:24 -0300

    fix(logo): replace Link component with anchor tag for improved accessibility

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,10 @@
 import { Wrench } from "lucide-react";
-import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Link href="/" className="flex items-center space-x-1.5">
+    <a href="/" className="flex items-center space-x-1.5">
       <Wrench className="size-4" />
       <span className="font-medium">Servicin</span>
-    </Link>
+    </a>
   );
 }
